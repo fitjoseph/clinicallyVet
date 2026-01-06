0a16a1fb15a95ff850a18d4a0878a6b9ce21c777
fitjoseph <103972275+fitjoseph@users.noreply.github.com>
2026-01-06 05:48:00 -0800

    Add generic date to footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -66,7 +66,7 @@ const Footer: React.FC = () => {
 
         <div className="border-t border-gray-800 mt-12 pt-8 text-center">
           <p className="text-gray-400">
-            © ClinicallyVet Group. All rights reserved. Made with ❤️ for pet lovers everywhere.
+            © 2024 ClinicallyVet Group. All rights reserved. Made with ❤️ for pet lovers everywhere.
           </p>
         </div>
       </div>
